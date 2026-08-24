7f453fa1b3082d364e1a9774c710fb4784714bc2
Lellosport <lorenzo.chiarello2008@gmail.com>
2026-08-24 19:27:52 +0200

    Create test_vulkan.cpp

---
+++ app/src/main/cpp/test_vulkan.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <MNN/Interpreter.hpp>
+
+int main() {
+    std::cout << "--- TEST MNN VULKAN INIZIATO ---" << std::endl;
+    MNN::ScheduleConfig config;
+    config.type = MNN_FORWARD_VULKAN;
+    config.numThread = 4;
+    std::cout << "Tentativo di inizializzazione..." << std::endl;
+    
+    auto runtime_pair = MNN::Interpreter::createRuntime({config});
+    
+    if (runtime_pair.second != nullptr) {
+        std::cout << "SUCCESSO: La GPU ha accettato Vulkan!" << std::endl;
+    } else {
+        std::cout << "FALLITO: Impossibile creare il Runtime Vulkan." << std::endl;
+    }
+    
+    return 0;
+}
